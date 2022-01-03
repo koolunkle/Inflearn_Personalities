71e18ac139aa6157078884667e6b30f7bfca452f
koolunkle <gugi329@daum.net>
2022-01-03 23:56:18 +0900

    NavGraph & NavController 정의

--- app/src/main/java/com/inflearn/lovepersonalitytest/MainActivity.kt
+++ app/src/main/java/com/inflearn/lovepersonalitytest/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.inflearn.lovepersonalitytest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navController = nav_host_fragment.findNavController()
     }
 }
